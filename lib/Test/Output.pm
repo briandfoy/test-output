0b55fe1f1aab8521f9efef12b5de7b1cb6b1b68d
ssoriche <ssoriche@1b406142-e1ef-0310-9597-f0ddb2e226e2>
2005-11-09 21:55:28 +0000

    r1243@Prowl:  ssoriche | 2005-11-09 16:57:24 -0500  Updated version number to 0.09 preparing for release.
    git-svn-id: http://svn.coloredblocks.net/svn/Test-Output/trunk@71 1b406142-e1ef-0310-9597-f0ddb2e226e2

--- lib/Test/Output.pm
+++ lib/Test/Output.pm
@@ -24,11 +24,11 @@ Test::Output - Utilities to test STDOUT and STDERR messages.
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 =head1 SYNOPSIS
 
